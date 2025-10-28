60b5d63274cfadba24c0d7187df9c4d6cbdcf3fc
Chystikov Maksim <maksim126723@gmail.com>
2025-10-28 23:01:12 +0200

    створення репозиторія для роботи з даними.

---
+++ TaskManagementService/Repository/InMemoryTaskRepository.cs
@@ -0,0 +1,30 @@
+﻿using TaskManagementService.Models;
+using TaskManagementService.Repository.Interface;
+
+namespace TaskManagementService.Repository
+{
+    public class InMemoryTaskRepository : ITaskRepository
+    {
+        private readonly List<TaskItem> _tasks = new();
+
+        public Task<IEnumerable<TaskItem>> GetAllAsync()
+            => Task.FromResult<IEnumerable<TaskItem>>(_tasks);
+
+        public Task<TaskItem?> GetByIdAsync(Guid id)
+            => Task.FromResult(_tasks.FirstOrDefault(t => t.Id == id));
+
+        public Task<TaskItem> AddAsync(TaskItem task)
+        {
+            _tasks.Add(task);
+            return Task.FromResult(task);
+        }
+
+        public Task UpdateAsync(TaskItem task)
+        {
+            var index = _tasks.FindIndex(t => t.Id == task.Id);
+            if (index >= 0)
+                _tasks[index] = task;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ TaskManagementService/Repository/Interface/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskManagementService.Models;
+
+namespace TaskManagementService.Repository.Interface
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<TaskItem>> GetAllAsync();
+        Task<TaskItem?> GetByIdAsync(Guid id);
+        Task<TaskItem> AddAsync(TaskItem task);
+        Task UpdateAsync(TaskItem task);
+    }
+
+}
